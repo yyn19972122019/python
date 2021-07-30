d3766757a2afc66c1cb9d9a01911dc174b064dc7
841345526@qq.com <841345526@qq.com>
2021-07-30 15:09:00 +0800

    测试报告添加

--- 1.py
+++ 1.py
@@ -1,8 +1,17 @@
-class a:
-    def __init__(self):
-        self.tt = 1
-class b(a):
-    def c(self):
-        print(self.tt)
+# class a:
+#     def __init__(self,year,month,day):
+#         self.year = year
+#         self.month = month
+#         self.day = day
+#     @property
+#     def pro(self):
+#         print(f'{self.year}年,{self.month}月,{self.day}日')
+#     @classmethod
+#     def ptt(cls,data):
+#         year,month,day = map(int,data.split('-'))
+#         date1 = cls(year,month,day)
+#         return date1
 
-b().c()
+
+if __name__ == '__main__':
+   1
--- Testcase/test_one.py
+++ Testcase/test_one.py
@@ -1,12 +1,14 @@
 
 import pytest,webui.web as web,data.datachange as data
-
+import allure
+@allure.feature("编辑页面")
 class TestLogin:
     # 无法使用--init--，因为pytest你不能去修改pytest类的构造属性
     # def __init__(self):
     #     self.login = webui.web.denglu().login()
     #     self.data = list(data.opxlrd())
     # 登录成功
+    @allure.title("登录成功")
     def test_01(self):
         datas = list(data.opxlrd())
         user, pwd, hope = datas[0].values()
@@ -15,6 +17,7 @@ class TestLogin:
             assert res == hope
         except AssertionError as faild:
             print('与预期结果不一致'+ faild)
+    @allure.title("账户或密码错误")
     def test_02(self):
         datas = list(data.opxlrd())
         user, pwd, hope = datas[1].values()
@@ -23,6 +26,7 @@ class TestLogin:
             assert res == hope
         except AssertionError as faild:
             print('与预期结果不一致'+ faild)
+    @allure.title("账号或密码长度不够")
     def test_03(self):
         datas = list(data.opxlrd())
         user, pwd, hope = datas[2].values()
